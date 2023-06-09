06f581c4ca226f9b32bee57208a33d521f3d1a36
Ayoub Bouassid <abouassi@e1r10p4.1337.ma>
2023-06-09 07:30:08 +0100

    abouassi

--- abouassi/cub3D.c
+++ abouassi/cub3D.c
@@ -6,52 +6,15 @@
 /*   By: abouassi <abouassi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/07 10:27:29 by abouassi          #+#    #+#             */
-/*   Updated: 2023/06/08 21:22:50 by abouassi         ###   ########.fr       */
+/*   Updated: 2023/06/09 07:29:18 by abouassi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../cub3d.h"
 
-void check_nwline(char *map)
+void	cheak_wall(char *map)
 {
-    int i;
-    int check;
-    int k;
-
-    k = 0;
-    check = 0;
-    i = 0;
-    while (map[i])
-    {
-        while (map[i] && map[i] != '\n')
-        {
-            i++;
-            k++;
-        }
-        if(k != 0)
-            check++;
-        k = 0;
-        if (!map[i] || check == 6)
-           break;
-        i++;
-    }
-	while (map[i] && map[i] == '\n')
-	{
-		i++;
-	}
-    while (map[i])
-	{
-		if (map[i] == '\n' && map[i + 1] == '\n')
-		{
-			ft_puterr("Error");
-		}
-		i++;
-	}
-	
-}
-void cheak_wall(char *map)
-{
-	int i;
+	int	i;
 	i  = 0;
 	while (map[i])
 	{
@@ -64,9 +27,9 @@ void cheak_wall(char *map)
 	}
 }
 
-void check_s(char **map,size_t i, size_t j)
+void	check_s(char **map,size_t i, size_t j)
 {
-	if (ft_strlen(map[i - 1]) <= j  || ft_strlen(map[i + 1]) <= j || j == 0)
+	if	(ft_strlen(map[i - 1]) <= j  || ft_strlen(map[i + 1]) <= j || j == 0)
 	{
 		ft_puterr("Error");
 	}
@@ -75,10 +38,11 @@ void check_s(char **map,size_t i, size_t j)
 		ft_puterr("Error");
 	}
 }
-void cheak_map(char **map)
+
+void	cheak_map(char **map)
 {
-	size_t i;
-	size_t j;
+	size_t	i;
+	size_t	j;
 
 	i = 1;
 	j = 0;
@@ -92,30 +56,31 @@ void cheak_map(char **map)
 		while (map[i][j])
 		{
 			if(map[i][j] == '0' || map[i][j] == 'W' || map[i][j] == 'E' || map[i][j] == 'N' || map[i][j] == 'S')
-				check_s(map,i,j);
+				check_s(map, i, j);
 			j++;
 		}
 		i++;
 	}
 }
+
 char **get_map(char *file)
 {
-	int fd;
-	char **map;
-	char *maping;
+	int		fd;
+	char	**map;
+	char	*maping;
 
-	fd = open(file,O_RDONLY);
+	fd = open(file, O_RDONLY);
 	if (fd == -1)
 	{
 		ft_puterr("Error");
 	}
 	maping = get_next_line(fd);
 	check_nwline(maping);
-	map = ft_split(maping,'\n');
-	return(map);
-	
+	map = ft_split(maping, '\n');
+	return (map);
 }
-int main(int ac ,char **av)
+
+int main(int ac , char **av)
 {
 	(void)ac;
 	char **map;
@@ -123,16 +88,13 @@ int main(int ac ,char **av)
 
 	i = 0;
 	map = get_map(av[1]);
-	check_first_element(map,i);
+	check_first_element(map, i);
 	cheak_map(&map[6]);
 	check_cmp(&map[6]);
 	check_cmp_valid(&map[6]);
 	while (map[i])
 	{
-		printf("%s\n",map[i]);
+		printf("%s\n", map[i]);
 		i++;
 	}
-	
-
-	
 }
--- abouassi/cub3D_parce.c
+++ abouassi/cub3D_parce.c
@@ -6,55 +6,102 @@
 /*   By: abouassi <abouassi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/07 10:27:39 by abouassi          #+#    #+#             */
-/*   Updated: 2023/06/08 20:44:45 by abouassi         ###   ########.fr       */
+/*   Updated: 2023/06/09 07:27:09 by abouassi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../cub3d.h"
 
-void check_cmp(char **map)
+void	check_cmp(char **map)
 {
-    int i;
-    int j;
-    int check;
-    check = 0;
-    i = 0;
-    j = 0;
-    while (map[i])
-    {
-        j = 0;
-        while (map[i][j])
-        {
-            if (map[i][j] == 'W' || map[i][j] == 'E' || map[i][j] == 'N' || map[i][j] == 'S')
-            {
-                if (check)
-                   ft_puterr("Error");
-                check++;
-            }
-            j++;
-        }
-        i++;   
-    }
-    if (!check)
-        ft_puterr("Error\n<!> no PLAYER <!>");
-}
-void check_cmp_valid(char **map)
-{
-    int i;
-    int j;
+	int	i;
+	int	j;
+	int	check;
 
-    i = 0;
-    while (map[i])
-    {
-        j = 0;
-        while (map[i][j])
-        {
-            if (map[i][j] != '0' && map[i][j] != '1' && map[i][j] != 'W' && map[i][j] != 'S' && map[i][j] != 'N' && map[i][j] != 'E' && map[i][j] != ' ')
-            {
-                ft_puterr("Error");
-            }
-            j++;
-        }
-        i++;
-    }
+	check = 0;
+	i = 0;
+	j = 0;
+	while (map[i])
+	{
+		j = 0;
+		while (map[i][j])
+		{
+			if (map[i][j] == 'W' || map[i][j] == 'E'
+				|| map[i][j] == 'N' || map[i][j] == 'S')
+			{
+				if (check)
+					ft_puterr("Error");
+				check++;
+			}
+			j++;
+		}
+		i++;
+	}
+	if (!check)
+		ft_puterr("Error\n<!> no PLAYER <!>");
+}
+
+void	check_cmp_valid(char **map)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (map[i])
+	{
+		j = 0;
+		while (map[i][j])
+		{
+			if (map[i][j] != '0' && map[i][j] != '1' && map[i][j] != 'W'
+				&& map[i][j] != 'S' && map[i][j] != 'N' && map[i][j] != 'E'
+					&& map[i][j] != ' ')
+			{
+				ft_puterr("Error");
+			}
+			j++;
+		}
+		i++;
+	}
+}
+
+void	check_mult_new(char *map, int i)
+{
+	while (map[i] && map[i] == '\n')
+	{
+		i++;
+	}
+	while (map[i])
+	{
+		if (map[i] == '\n' && map[i + 1] == '\n')
+		{
+			ft_puterr("Error");
+		}
+		i++;
+	}	
+}
+
+void	check_nwline(char *map)
+{
+	int	i;
+	int	check;
+	int	k;
+
+	k = 0;
+	check = 0;
+	i = 0;
+	while (map[i])
+	{
+		while (map[i] && map[i] != '\n')
+		{
+			i++;
+			k++;
+		}
+		if (k != 0)
+			check++;
+		k = 0;
+		if (!map[i] || check == 6)
+			break ;
+		i++;
+	}
+	check_mult_new(map, i);
 }
