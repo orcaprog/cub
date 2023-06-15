53ec515bcc290ce2e7dee122ca9313b9eaafb48c
Ayoub Bouassid <abouassi@e1r10p4.1337.ma>
2023-06-15 15:53:46 +0100

    abouassi

--- cub3d.h
+++ cub3d.h
@@ -6,7 +6,7 @@
 /*   By: abouassi <abouassi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/07 10:27:45 by abouassi          #+#    #+#             */
-/*   Updated: 2023/06/15 10:05:23 by abouassi         ###   ########.fr       */
+/*   Updated: 2023/06/15 15:52:45 by abouassi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -75,7 +75,7 @@ void	move_up(t_info *data);
 void	move_down(t_info *data);
 int		input_key(int key, t_info *data);
 int		prm_moves(char **map,int x ,int y);
-void	find_cord_x(t_info *data);
+t_point	find_cord_x(t_info *data);
 
 // aelidrys
 void	ft_error(void);
