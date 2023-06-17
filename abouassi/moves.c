7252a4fbd61aac8c05a7e742db87550a12fcb476
Ayoub Bouassid <abouassi@e1r8p11.1337.ma>
2023-06-17 22:47:09 +0100

    abouassi

--- abouassi/moves.c
+++ abouassi/moves.c
@@ -6,89 +6,12 @@
 /*   By: abouassi <abouassi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/10 14:42:41 by abouassi          #+#    #+#             */
-/*   Updated: 2023/06/17 22:33:41 by abouassi         ###   ########.fr       */
+/*   Updated: 2023/06/17 22:47:04 by abouassi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../cub3d.h"
-t_point	point_x(t_info *cub, double cor_rd)
-{
-	t_point	p;
-	double	dx;
-	double	dy;
 
-	p.y = cub->y;
-	if (cub->y_d == -1)
-    p.y = (floor(cub->y / 100) * 100) + 100;
-    if (cub->y_d == 1)
-    {
-        p.y = (floor(p.y / 100) * 100) + 100;
-        dy = p.y - cub->y;
-        dx = (dy / tan(cor_rd));
-        p.x = cub->x - dx;
-        if ((int)p.x % 100 >= 98)
-            p.x = (floor(p.x / 100) + 1) * 100;
-    }
-    if (cub->y_d == -1)
-    {
-        p.y -= 100;
-        dy = (cub->y - p.y);
-        dx = (dy / tan(cor_rd));
-        p.x = cub->x + dx;
-        // printf("y_d = -1 => dy = %lf & dx = %lf\n", dy, dx);
-    }
-    // printf("p_x = %lf\n", p.x);
-    // printf("p_y = %lf\n", p.y);
-    // if ((int)(p.x) % 100 == 0 && (int)(p.y) % 100 == 0 && cub->x_d == -1)
-    //     kx = 1;
-		// printf("is_in_map_range = %d\n\n", is_coord_in_map_range(cub, p.x, p.y));
-
-	p.r = sqrt(pow(floor(cub->x - p.x),2) + pow(floor(cub->y - p.y),2));
-	if (p.r < 0)
-		p.r *= -1;
-	if (p.r == -2147483648)
-		p.r = p.r + 1 * -1;
-	// printf("{p1.r = %lf}\n",p.r);
-	return (p);
-}
-t_point	point_y(t_info *cub, double cor_rd)
-{
-    t_point	p;
-	double	dx;
-	double	dy;
-
-	p.y = cub->y;
-	if (cub->y_d == -1)
-		p.y = (floor(cub->y / 100) * 100) + 100;
-    if (cub->y_d == 1)
-    {
-        p.y = (floor(p.y / 100) * 100) + 100;
-        dy = p.y - cub->y;
-        dx = (dy / tan(cor_rd));
-        p.x = cub->x - dx;
-        // if ((int)p.x % 100 >= 98)
-        // 	p.x = (floor(p.x / 100) + 1) * 100;
-    }
-    if (cub->y_d == -1)
-    {
-
-        p.y -= 100;
-        dy = (cub->y - p.y);
-        dx = (dy / tan(cor_rd));
-        p.x = cub->x + dx;
-        // printf("y_d = -1 => dy = %lf & dx = %lf\n", dy, dx);
-    }
-    // printf("p_x = %lf\n", p.x);
-    // printf("p_y = %lf\n", p.y);
-    // printf("is_in_map_range = %d\n\n", is_coord_in_map_range(cub, p.x, p.y));
-	p.r = sqrt(pow(floor(cub->x - p.x),2) + pow(floor(cub->y - p.y),2));
-	if (p.r < 0)
-		p.r *= -1;
-	if (p.r == -2147483648)
-		p.r = p.r + 1 * -1;
-	// printf("{p1.r = %lf}\n",p.r);
-	return (p);
-}
 int prm_moves(char **map,int x ,int y)
 {
      printf("|x = %d y = %d |\n",x ,y);
