4e50c6f2dec2852ef22bc22e23694054c8831631
Ayoub Elidryssy <aelidrys@e1r10p5.1337.ma>
2023-06-24 13:32:06 +0100

    aelidrys

--- aelidrys/main.c
+++ aelidrys/main.c
@@ -6,7 +6,7 @@
 /*   By: aelidrys <aelidrys@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/08 10:26:03 by aelidrys          #+#    #+#             */
-/*   Updated: 2023/06/24 13:13:27 by aelidrys         ###   ########.fr       */
+/*   Updated: 2023/06/24 13:24:36 by aelidrys         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,14 +54,14 @@ int main(int ac, char **av)
 		ft_error();
 	cub->img[0].ptr = mlx_new_image(cub->mlx->ptr, 1200,1000);
 	cub->img[1].ptr = mlx_new_image(cub->mlx->ptr, 1200,1000);
-	cub->img[2].addr =  mlx_get_img_addr(cub->img[2].img, &cub->img[2].bits_per_pixel,
+	cub->img[2].addr =  mlx_get_data_addr(cub->img[2].ptr, &cub->img[2].bits_per_pixel,
 		&cub->img[2].line_length, &cub->img[2].endian);
-	cub->img[3].addr =  mlx_get_img_addr(cub->img[3].img, &cub->img[3].bits_per_pixel,
+	cub->img[3].addr =  mlx_get_data_addr(cub->img[3].ptr, &cub->img[3].bits_per_pixel,
 		&cub->img[3].line_length, &cub->img[3].endian);
-	cub->img[4].addr =  mlx_get_img_addr(cub->img[4].img, &cub->img[4].bits_per_pixel,
+	cub->img[4].addr =  mlx_get_data_addr(cub->img[4].ptr, &cub->img[4].bits_per_pixel,
 		&cub->img[4].line_length, &cub->img[4].endian);
-	cub->img[0].addr = mlx_get_img_addr(cub->img[0].img, &cub->img[0].bits_per_pixel,&cub->img[0].line_length, &cub->img[0].endian);
-	cub->img[1].addr = mlx_get_img_addr(cub->img[1].img, &cub->img[1].bits_per_pixel,&cub->img[1].line_length, &cub->img[1].endian);
+	cub->img[0].addr = mlx_get_data_addr(cub->img[0].ptr, &cub->img[0].bits_per_pixel,&cub->img[0].line_length, &cub->img[0].endian);
+	cub->img[1].addr = mlx_get_data_addr(cub->img[1].ptr, &cub->img[1].bits_per_pixel,&cub->img[1].line_length, &cub->img[1].endian);
 	draw_simple_map(cub);
 	mlx_hook(cub->mlx->win, 17, 0, ft_exit, NULL);
 	mlx_hook(cub->mlx->win,2,0,a_event,cub);
